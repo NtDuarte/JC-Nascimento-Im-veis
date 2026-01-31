7df5f8c39bd9bd3a2ee0a623f27ef8fcdd809fe5
NtDuarte <northon.luizz@gmail.com>
2026-01-31 10:25:34 -0300

    Update script.js

---
+++ imobiliaria-site/script.js
@@ -0,0 +1,20 @@
+const imoveis = [
+    {
+        id: 1,
+        titulo: "Apartamento aconchegante em Nova Iguaçu",
+        preco: "R$ 250.000",
+        quartos: 2,
+        vagas: 1,
+        imagem: "img/imovel1.jpg", // Nome da foto que você vai salvar na pasta img
+        linkZap: "https://wa.me/5521999999999?text=Vi%20o%20imovel%201%20e%20gostei"
+    },
+    {
+        id: 2,
+        titulo: "Casa Ampla com Quintal",
+        preco: "R$ 450.000",
+        quartos: 3,
+        vagas: 2,
+        imagem: "img/imovel2.jpg",
+        linkZap: "https://wa.me/5521999999999?text=Quero%20saber%20mais%20da%20casa"
+    }
+];
